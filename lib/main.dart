46b1ca2e81b7fd1d49ee368e5f7c45ae01da90b6
rishabh991 <rishabh.qc991@gmail.com>
2021-02-08 16:30:58 +0530

    Title bar colour change

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Research',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Research'),
     );
